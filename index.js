123764314ae87388d710d05ebe2f2fc1e1170a61
Anton Deviun <a.deviun@gmail.com>
2017-12-06 07:19:53 +0300

    fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const ROOT = __dirname;
-const moduleName = 'jmongo.index';
-
-const $path = require('path');
-
-module.exports = require($path.resolve(ROOT, 'src/jmongo'));
+module.exports = require('./src/jmongo');
