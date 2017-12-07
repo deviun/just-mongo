0aed9363c86ddba9e29c181d9d27a67e8b50a9f5
Anton Deviun <a.deviun@gmail.com>
2017-12-07 14:07:43 +0300

    first test listen

---
+++ examples/listen.test.js
@@ -0,0 +1,88 @@
+const $JMongo = require('../');
+
+const $log = require('../src/libs/log');
+const $Promise = require('bluebird');
+
+// пример моделей для коллекций
+const models = {
+  feed: {
+    data: String,
+    time: Number,
+    index: Number
+  }
+};
+
+const $mongo = new $JMongo({
+  models,
+  log: 'info',
+  db: 'jmongo'
+}, function (err, ok) {
+  if (err) {
+    $log.info(err);
+  } else {
+    $log.info({ok});
+  }
+});
+
+const feedDB = $mongo.collection('feed');
+
+let i = 0;
+
+setInterval(() => {
+  feedDB.insert({
+    data: `Current time: ${(new Date().getTime())}`,
+    index: ++i, 
+    time: (new Date()).getTime()
+  });
+}, 500);
+
+let firstCheckTime;
+
+(async () => {
+  const subs = feedDB.listen(async (lastUpdates) => {
+    if (!lastUpdates && !firstCheckTime) {
+      firstCheckTime = await feedDB.findOne(false, {
+        sort: {
+          time: -1
+        }
+      });
+
+      lastUpdates = [firstCheckTime];
+    } else if (!lastUpdates && firstCheckTime) {
+      lastUpdates = [firstCheckTime];
+    }
+
+    return await feedDB.find({
+      time: {
+        $gt: lastUpdates.pop().time
+      }
+    }, {
+      project: {
+        _id: 0,
+        time: 1,
+        index: 1,
+        data: 1
+      }
+    });
+  }, 1000);
+
+  subs
+    .error((err) => {
+      $log.error(err);
+    })
+    .addListener((updates) => {
+      $log.info({updates});
+    });
+
+  setTimeout(() => {
+    subs.close();
+
+    $log.info('listen stop');
+
+    process.exit();
+  }, 5000);
+})()
+.catch((err) => {
+  $log.error(err);
+  process.exit();
+});
---
+++ src/engines/listen/index.js
@@ -0,0 +1,73 @@
+const ROOT = `${__dirname}/../../../`;
+const moduleName = 'jmongo.engines.listen';
+
+const EventEmitter = require('events');
+const _ = require('lodash');
+
+class Listen extends EventEmitter {
+  constructor (getUpdates, timeout = 250) {
+    super();
+
+    Object.assign(this, {
+      getUpdates,
+      timeout,
+      lastUpdates: false,
+      stop: false,
+      errorHandler: (err) => console.log(err)
+    });
+
+    this._check();
+  }
+
+  async _check () {
+    if (this.stop) {
+      return;
+    }
+
+    try {
+      const updates = await this.getUpdates(this.lastUpdates)
+        .catch(this.errorHandler);
+
+      const isSend = typeof updates === 'string' ? updates.length !== 0 :
+        typeof updates === 'number' ? updates !== 0 :
+        updates instanceof Array ? updates.length !== 0 :
+        typeof updates === 'object' ? updates && Object.keys(updates).length !== 0 :
+        typeof updates === 'boolean' ? updates : true;
+
+      if (isSend) {
+        this.lastUpdates = updates;
+        super.emit('updates', updates);
+      }
+
+      setTimeout(this._check.bind(this), this.timeout);
+    } catch (err) {
+      this.errorHandler(err);
+    }
+  }
+
+  addListener (cb) {
+    super.addListener('updates', cb);
+
+    return this;
+  }
+
+  removeListener (cb) {
+    super.removeListener('updates', cb);
+
+    return this;
+  }
+
+  close () {
+    super.removeAllListeners();
+    this.stop = true;
+  }
+
+  error (cb) {
+    this.errorHandler = cb;
+
+    return this;
+  }
+  
+}
+
+module.exports = Listen;
