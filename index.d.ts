165bfa51bc399d189592f57c3ada23dfe890daab
deviun <a.deviun@gmail.com>
2019-10-13 13:30:52 +0300

    improve types

--- index.d.ts
+++ index.d.ts
@@ -38,20 +38,20 @@ declare module "just-mongo" {
 
   export class Collection {
     insert(list: any[] | any, options?: any): Promise<any>
-    deleteMany<FT>(filter: FT | MongoFilter, options?: any): Promise<any>
-    deleteOne<FT>(filter: FT | MongoFilter, options?: any): Promise<any>
-    count(filter?: MongoFilter, options?: any): Promise<number>
+    deleteMany<FT = MongoFilter>(filter: FT, options?: any): Promise<any>
+    deleteOne<FT = MongoFilter>(filter: FT, options?: any): Promise<any>
+    count<FT = MongoFilter>(filter?: FT, options?: any): Promise<number>
     ObjectID(id: string): any
-    findOne<RT, FT>(filter?: RT | FT | MongoFilter, options?: any): Promise<RT>
-    find<RT, FT>(filter?: RT | FT | MongoFilter, options?: any): Promise<RT>
-    updateOne<FT>(filter: FT | MongoFilter, update: any, options?: any): Promise<any>
-    updateMany<FT>(filter: FT | MongoFilter, update: any, options?: any): Promise<any>
-    editOne<FT>(filter: FT | MongoFilter, update: any, options?: any): Promise<any>
-    editMany<FT>(filter: FT | MongoFilter, update: any, options?: any): Promise<any>
-    aggregate<RT, PLT>(pipeline: PLT | PipelineItem[], options?: any): Promise<RT>
+    findOne<RT, FT = RT | MongoFilter>(filter?: RT, options?: any): Promise<RT>
+    find<RT, FT = RT | MongoFilter>(filter?: FT, options?: any): Promise<RT>
+    updateOne<FT = MongoFilter>(filter: FT | MongoFilter, update: any, options?: any): Promise<any>
+    updateMany<FT = MongoFilter>(filter: FT | MongoFilter, update: any, options?: any): Promise<any>
+    editOne<FT = MongoFilter>(filter: FT, update: any, options?: any): Promise<any>
+    editMany<FT = MongoFilter>(filter: FT, update: any, options?: any): Promise<any>
+    aggregate<RT, PLT = PipelineItem[]>(pipeline: PLT, options?: any): Promise<RT>
     native(cb: Function): Promise<NativeInterface>
-    findRandom<RT, FT>(filter: FT | MongoFilter, count: number, options?: any): Promise<RT>
-    join<RT, FT>(filter: FT | MongoFilter, joinCollection: Collection, joinFiled: joinFiledT, project: any, options?: any): Promise<RT>
+    findRandom<RT, FT = MongoFilter>(filter: FT, count: number, options?: any): Promise<RT>
+    join<RT, FT = MongoFilter>(filter: FT, joinCollection: Collection, joinFiled: joinFiledT, project: any, options?: any): Promise<RT>
     listen(getUpdates: Function, timeout?: number): Listen
   }
 
